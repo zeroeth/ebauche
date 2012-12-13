b53586c4b66813fb8e9c605d8939b96aef16c095
Kevin Alford <zeroeth@pixelflow.org>
2012-12-13 01:11:52 -0600

    Reorganize skeleton

--- ebauche.js
+++ ebauche.js
@@ -6,25 +6,24 @@ $(document).ready(function () {
   var camera   = new THREE.PerspectiveCamera(angle, aspect, near, far);
   var scene    = new THREE.Scene();
 
-  scene.add(camera);
-  camera.position.z = 300;
-  renderer.setSize(width, height);
-  $('#container').append(renderer.domElement);
-
-
   var material = new THREE.MeshNormalMaterial();
   var geometry = new THREE.CylinderGeometry(10, 10, 10, 16, 1, false);
   var sphere   = new THREE.Mesh(geometry, material);
 
-  scene.add(sphere);
-
-
   var light    = new THREE.PointLight(0xFFFFFF);
 
-  scene.add(light);
+
+  camera.position.z = 300;
+
   light.position.x = 10;
   light.position.y = 50;
   light.position.z = 130;
 
+  scene.add(camera);
+  scene.add(sphere);
+  scene.add(light);
+
+  $('#container').append(renderer.domElement);
+  renderer.setSize(width, height);
   renderer.render(scene, camera);
 });
