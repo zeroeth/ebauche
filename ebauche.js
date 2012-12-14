400280edd92df5c242c4d50051126cb531e4183f
Kevin Alford <zeroeth@pixelflow.org>
2012-12-14 15:52:56 -0600

    Add second hand and comments

--- ebauche.js
+++ ebauche.js
@@ -1,34 +1,49 @@
 $(document).ready(function () {
+
+  // Dimension Settings
   var width = 400, height = 300;
   var angle = 45, aspect = width/height, near = 0.1, far = 10000;
 
+
+  // World Objects
   var renderer = new THREE.WebGLRenderer();
   var camera   = new THREE.PerspectiveCamera(angle, aspect, near, far);
   var scene    = new THREE.Scene();
-
-  var material = new THREE.MeshNormalMaterial();
-  var geometry = new THREE.CylinderGeometry(10, 10, 10, 16, 1, false);
-  var cylinder = new THREE.Mesh(geometry, material);
-
   var light    = new THREE.PointLight(0xFFFFFF);
+  var material = new THREE.MeshNormalMaterial();
+
+  var geometry = new THREE.CylinderGeometry(20, 21, 4, 24, 1, false);
+  var dial     = new THREE.Mesh(geometry, material);
+
+  var geometry = new THREE.CubeGeometry(40, 1, 3);
+  var second   = new THREE.Mesh(geometry, material);
+
+  // Positions
+  dial.position.x   = 0;
+  dial.position.y   = 0;
+  dial.position.z   = 0;
+
+  second.position.x = 0;
+  second.position.y = 3;
+  second.position.z = 0;
+
+  light.position.x  = 10;
+  light.position.y  = 50;
+  light.position.z  = 130;
+
+  camera.position.z = 40;
+  camera.position.y = 40;
+  camera.lookAt(dial.position);
 
 
-  cylinder.position.x = 0;
-  cylinder.position.y = 0;
-  cylinder.position.z = 0;
-
-  light.position.x = 10;
-  light.position.y = 50;
-  light.position.z = 130;
-
-  camera.position.z = 30;
-  camera.position.y = 30;
-  camera.lookAt(cylinder.position);
-
+  // Build World
   scene.add(camera);
-  scene.add(cylinder);
   scene.add(light);
+  scene.add(dial);
+  scene.add(second);
 
+
+  // Render
   $('#container').append(renderer.domElement);
   renderer.setSize(width, height);
   renderer.render(scene, camera);
